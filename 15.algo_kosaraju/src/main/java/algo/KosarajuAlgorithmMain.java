a25ff49d9b52f51bc18b9cb3b9d64545f29c7958
ya-rko <61264356+ya-rko@users.noreply.github.com>
2020-07-17 02:09:21 +0500

    move main class (task 15, 16)

---
+++ 15.algo_kosaraju/src/main/java/algo/KosarajuAlgorithmMain.java
@@ -0,0 +1,41 @@
+package algo;
+
+
+import algo.graph.KosarajuAlgorithm;
+
+import java.util.Arrays;
+
+
+public class KosarajuAlgorithmMain {
+    public static void main(String[] args) {
+        int[][] adjacency = {
+                {1},
+                {2, 4, 5},
+                {3, 6},
+                {2, 7},
+                {0, 5},
+                {6},
+                {5},
+                {3, 6}
+        };
+
+        int[] connectedComponents = KosarajuAlgorithm.run(adjacency);
+        printConnectedComponents(connectedComponents);
+    }
+
+
+    private static void printConnectedComponents(int[] connectedComponents) {
+        int maxIndex = Arrays.stream(connectedComponents).max().getAsInt();
+
+        for (int i = 1; i <= maxIndex; i++) {
+            System.out.println("Сonnected сomponent #" + i + ":");
+
+            for (int c = 0; c < connectedComponents.length; c++) {
+                if (connectedComponents[c] == i) {
+                    System.out.print(c + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
